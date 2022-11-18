b6ea1b22f64f0334d957c44144d0f6f16a91eb49
HellaMol <111105393+HellaMol@users.noreply.github.com>
2022-11-18 02:29:28 -0600

    Add files via upload

--- mainjuego.cpp
+++ mainjuego.cpp
@@ -4,9 +4,28 @@
 
 int main(){
 
-    conejito conejito_1("blanco", "bombon", 100, 100);
-    gatito gatito_1("esfinge", "bingus", 100, 100);
-    std::cout << conejito_1.get_razaCON()<< endl;
-    std::cout << gatito_1.get_nombreGAT();
+    //mago
+    Personaje personaje_1("Hu Dhin I", 20, 50, 2);
+    //caballero
+    Personaje personaje_2("Laurel de Carim", 30, 100, 3);
+    //asesino
+    Personaje personaje_3("Illona", 35, 85, 4);
+    //mago
+    Personaje personaje_4("Howl", 20, 60, 3);
+    //asesino
+    Personaje personaje_5("Gilles", "", 50, 70, 2);
+    //tanque
+    Personaje personaje_6("Warren", 10, 150, 1);
+    //godin
+    Personaje personaje_7("Diego", 50, 40, 1);
+
+    Arma arma_1("arco", "destructora", 10);
+    Oponente oponente_1("Capitalismo", 130, 10);
+
+    std::cout << "Bienvenidx a la aventura"<< endl;
+    std::cout << "Selecciona un guerrero"<< endl;
+    std::cout << personaje_1.get_nombrePER()<< endl;
+    std::cout << arma_1.get_lvldanoAR()<< endl;
+    std::cout << oponente_1.get_lvlvidaOP();
 
 };
